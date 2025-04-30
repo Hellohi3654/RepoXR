d0248945fdfc21a9bd021625e044203b0dac387b
DaXcess <daxcess@daxcess.io>
2025-04-30 08:48:28 +0200

    Optimize vignette

--- Source/Rendering/CustomVignette.cs
+++ Source/Rendering/CustomVignette.cs
@@ -13,12 +13,6 @@ public class CustomVignette : PostProcessEffectSettings
     public override bool IsEnabledAndSupported(PostProcessRenderContext context)
         => enabled.value && intensity.value > 0f;
 
-    [Tooltip("Use the \"Classic\" mode for parametric controls. Use the \"Masked\" mode to use your own texture mask.")]
-    public VignetteModeParameter mode = new()
-    {
-        value = VignetteMode.Classic
-    };
-
     [Tooltip("Vignette color.")]
     public ColorParameter color = new()
     {
@@ -51,55 +45,23 @@ public class CustomVignette : PostProcessEffectSettings
     {
         value = false
     };
-
-    [Tooltip("A black and white mask to use as a vignette.")]
-    public TextureParameter mask = new()
-    {
-        value = null
-    };
-
-    [Range(0f, 1f)]
-    [Tooltip("Mask opacity.")]
-    public FloatParameter opacity = new()
-    {
-        value = 1f
-    };
 }
 
 [Preserve]
 public class CustomVignetteRenderer : PostProcessEffectRenderer<CustomVignette>
 {
-    private static readonly int VignetteMode = Shader.PropertyToID("_Vignette_Mode");
     private static readonly int VignetteColor = Shader.PropertyToID("_Vignette_Color");
     private static readonly int VignetteSettings = Shader.PropertyToID("_Vignette_Settings");
-    private static readonly int VignetteMask = Shader.PropertyToID("_Vignette_Mask");
-    private static readonly int VignetteOpacity = Shader.PropertyToID("_Vignette_Opacity");
-    private static readonly int VignetteActiveEye = Shader.PropertyToID("_Vignette_Active_Eye");
 
     public override void Render(PostProcessRenderContext context)
     {
         var sheet = context.propertySheets.Get(AssetCollection.VignetteShader);
+        var num = (1f - settings.roundness.value) * 6f + settings.roundness.value;
 
         sheet.properties.SetColor(VignetteColor, settings.color.value);
-        sheet.properties.SetFloat(VignetteActiveEye, context.xrActiveEye);
-
-        if (settings.mode == UnityEngine.Rendering.PostProcessing.VignetteMode.Classic)
-        {
-            var num = (1f - settings.roundness.value) * 6f + settings.roundness.value;
-
-            sheet.properties.SetFloat(VignetteMode, 0f);
-            sheet.properties.SetVector(VignetteSettings,
-                new Vector4(settings.intensity.value * 3f, settings.smoothness.value * 5f, num,
-                    settings.rounded.value ? 1f : 0f));
-
-            context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
-
-            return;
-        }
-
-        sheet.properties.SetFloat(VignetteMode, 1f);
-        sheet.properties.SetTexture(VignetteMask, settings.mask.value);
-        sheet.properties.SetFloat(VignetteOpacity, Mathf.Clamp01(settings.opacity.value));
+        sheet.properties.SetVector(VignetteSettings,
+            new Vector4(settings.intensity.value * 3f, settings.smoothness.value * 5f, num,
+                settings.rounded.value ? 1f : 0f));
 
         context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
     }
